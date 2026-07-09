6cb3e7b3f93e24af4689973fac23f473a2c77e5e
OrangeBlue2336 <snackcandy105@gmail.com>
2026-07-10 02:33:33 +0900

    10초 -> 15초

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const {
 const dayjs = require('dayjs');
 
 const RESPONSIBLE_GAMBLING_STATUS = '도박 중독 상담 및 신고는 국번 없이 1336';
-const PRESENCE_UPDATE_INTERVAL_MS = 10_000;
+const PRESENCE_UPDATE_INTERVAL_MS = 15_000;
 
 const CUSTOM_IDS = {
   meetSelect: 'ticket:meet',
